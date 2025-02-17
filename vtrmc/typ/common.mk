dbd78360dbcb104497543254f7cdba90e2dbc387
Misha Brukman <misha.brukman@gmail.com>
2025-02-17 18:19:49 -0500

    Add `common.typ` and `common.mk` as target deps
    Ensure we rebuild the PDF files whenever any of the common files change as well
    as the direct source files.

--- vtrmc/typ/common.mk
+++ vtrmc/typ/common.mk
@@ -5,18 +5,21 @@ endif
 
 TYPST ?= typst
 
+TYPST_DEPS = ../common.typ
+MAKEFILE_DEPS = Makefile ../common.mk
+
 all: problems solutions
 
 problems: problems.pdf
 
 PROBLEMS = $(ROOT)/third_party/vtrmc/typ/$(YEAR)/problem?.typ
 
-problems.pdf: problems.typ problem?.typ Makefile $(PROBLEMS)
+problems.pdf: problems.typ problem?.typ $(PROBLEMS) $(TYPST_DEPS) $(MAKEFILE_DEPS)
 	$(VERB) $(TYPST) --root="$(ROOT)" compile $< $@
 
 solutions: solutions.pdf
 
-solutions.pdf: solutions.typ solution?.typ Makefile $(PROBLEMS)
+solutions.pdf: solutions.typ solution?.typ $(PROBLEMS) $(TYPST_DEPS) $(MAKEFILE_DEPS)
 	$(VERB) $(TYPST) --root="$(ROOT)" compile $< $@
 
 clean:
